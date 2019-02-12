0a415d877f100fe1102c80e4f08532c26807164e
VasilevAlexey <Lexich.3d@gmail.com>
2019-02-12 14:12:58 +0300

    Sum in same page

--- action.php
+++ action.php
@@ -1,6 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title> test </title>
+</head>
+<body>
+<form action="action.php" method="post">
+    <p>первое число: <input type="text" name="a" /></p>
+    <p>второе число: <input type="text" name="b" /></p>
+    <p><input type="submit" name="sum" value="GO" /></p>
+</form>
 <?php
-$a = (int)$_POST['a'];
-$b = (int)$_POST['b'];
-$c = $a + $b;
-echo $c;
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['sum'])) {
+    func();
+}
+function func() {
+    $a = (int)$_POST['a'];
+    $b = (int)$_POST['b'];
+    $c = $a + $b;
+    echo $c;
+}
 ?>
+</body>
+</html>
